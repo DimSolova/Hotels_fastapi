93f3e59355abb52cd7fdac6f9c6ea4331d1e7515
Solova Mac <dima@MacBook-Air-Dima.local>
2025-08-18 19:11:40 +0300

    Реализовали пагинацию с Dependencies

--- hotels.py
+++ hotels.py
@@ -1,4 +1,9 @@
+from typing import Annotated
+
 from fastapi import Query,APIRouter, Body
+from fastapi.params import Depends
+
+from dependencies import PaginationParams, PaginationDep
 from schemas.hotels import Hotel, HotelPATCH
 
 router = APIRouter(prefix='/hotels', tags=['Отели'])
@@ -7,15 +12,21 @@ router = APIRouter(prefix='/hotels', tags=['Отели'])
 
 
 hotels = [
-    {'id': 1, 'title': 'Sochi', 'name': 'sochi'},
-    {'id': 2, 'title': 'Dubai', 'name': 'dubai'},
+    {"id": 1, "title": "Sochi", "name": "sochi"},
+    {"id": 2, "title": "Дубай", "name": "dubai"},
+    {"id": 3, "title": "Мальдивы", "name": "maldivi"},
+    {"id": 4, "title": "Геленджик", "name": "gelendzhik"},
+    {"id": 5, "title": "Москва", "name": "moscow"},
+    {"id": 6, "title": "Казань", "name": "kazan"},
+    {"id": 7, "title": "Санкт-Петербург", "name": "spb"},
 ]
 
 
 @router.get('')
 def get_hotels(
+        pagination: PaginationDep,
         id: int | None = Query(None, description='Id'),
-        title: str | None = Query(None, description='Hotel name')
+        title: str | None = Query(None, description='Hotel name'),
 ):
     hotels_ = []
     for hotel in hotels:
@@ -24,6 +35,8 @@ def get_hotels(
         if title and hotel['title'] != title:
             continue
         hotels_.append(hotel)
+    if pagination.page and pagination.per_page:
+        return hotels_[pagination.per_page * (pagination.page - 1):][:pagination.per_page]
     return hotels_
 
 
