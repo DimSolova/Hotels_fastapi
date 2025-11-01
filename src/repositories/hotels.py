d46e43be713265d93291d3ebb981736118c69602
Solova Mac <dima@MacBook-Air-Dima.local>
2025-11-01 17:49:52 +0300

    Реализовали Получение Свобоных отелей без Пагинации

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,7 +1,13 @@
+from datetime import date
+
+from dns.e164 import query
 from sqlalchemy import select,func
+
 from src.models.hotels import HotelsOrm
+from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
 from src.schemas.hotels import Hotel
+from src.repositories.utils import rooms_ids_for_booking
 
 
 class HotelsRepository(BaseRepository):
@@ -27,3 +33,16 @@ class HotelsRepository(BaseRepository):
         )
         result = await self.session.execute(query)
         return [self.schema.model_validate(model, from_attributes=True) for model in result.scalars().all()]
+
+    async def get_filtered_by_time(
+            self,
+            date_from: date,
+            date_to: date
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
