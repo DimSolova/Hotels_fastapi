c136de8d22caa9a33a1a8ccb85e90e0f0781d72b
Solova Mac <dima@MacBook-Air-Dima.local>
2025-11-01 10:49:38 +0300

    Реализовали проверку по отелю

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -9,8 +9,12 @@ class BaseRepository():
         self.session = session
 
     #Метод .filter_by позволяет распокавать словарь и применить метод where
-    async def get_filtered(self, **filter_by):
-        query = select(self.model).filter_by(**filter_by)
+    async def get_filtered(self, *filter,**filter_by):
+        query = (
+            select(self.model)
+            .filter(*filter)
+            .filter_by(**filter_by)
+        )
         result = await self.session.execute(query)
         return [self.schema.model_validate(model, from_attributes=True) for model in result.scalars().all()]
 
--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -21,39 +21,18 @@ class RoomsRepository(BaseRepository):
             date_from: date,
             date_to: date,
     ):
-        print(hotel_id,date_from,date_to)
-        """
-        with rooms_count as (
-            select room_id,count(*) as rooms_booked from bookings
-            where date_from <= '2024-11-07' and date_to >= '2024-07-01'
-            group by room_id
-        ),
-        rooms_left_table as (
-        select rooms.id as room_id , quantity - coalesce(rooms_booked, 0) as rooms_left
-        from rooms
-        left join rooms_count on rooms.id = rooms_count.room_id
-        )
-        select * from rooms_left_table
-        where rooms_left > 0
-        """
-
-        '''select room_id,count(*) as rooms_booked from bookings
-            where date_from <= '2024-11-07' and date_to >= '2024-07-01'
-            group by room_id'''
 
         rooms_count = (
             select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
             .select_from(BookingsOrm)
             .filter(
-                BookingsOrm.date_from <= '2024-11-07',
-                BookingsOrm.date_to >= '2024-07-01'
+                BookingsOrm.date_from <= date_to,
+                BookingsOrm.date_to >= date_from
             )
             .group_by(BookingsOrm.room_id)
             .cte(name='rooms_count')
         )
-        """select rooms.id as room_id , quantity - coalesce(rooms_booked, 0) as rooms_left
-        from rooms
-        left join rooms_count on rooms.id = rooms_count.room_id"""
+
         rooms_left_table = (
             select(RoomsOrm.id.label("room_id"),
                    (RoomsOrm.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)).label("rooms_left"),
@@ -63,17 +42,24 @@ class RoomsRepository(BaseRepository):
             .cte(name="rooms_left_table")
         )
 
-        '''
-        select * from rooms_left_table
-        where rooms_left > 0
-        '''
-
-        query = (
-            select(rooms_left_table)
-            .select_from(rooms_left_table)
-            .filter(rooms_left_table.c.rooms_left > 0)
+        rooms_ids_for_hotel = (
+            select(RoomsOrm.id)
+            .select_from(RoomsOrm)
+            .filter_by(hotel_id=hotel_id)
+            .subquery(name='rooms_ids_for_hotel')
         )
-        print(query.compile(bind=engine, compile_kwargs={'literal_binds': True}))
+
+        rooms_ids_to_get = (
+            select(rooms_left_table.c.room_id)
+            .select_from(rooms_left_table)
+            .filter(
+                rooms_left_table.c.rooms_left > 0,
+                rooms_left_table.c.room_id.in_(rooms_ids_for_hotel)
+            )
+        )
+        # print(query.compile(bind=engine, compile_kwargs={'literal_binds': True}))
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
     #где используется эта функция
     # async def get_price(self,room_id: int):
     #     stmt = select(self.model.price).where(self.model.id==room_id)
