fec8cfd06fa5fc8cc48d99ebe1de6b5622d4f665
Solova Mac <dima@MacBook-Air-Dima.local>
2026-02-20 20:55:04 +0300

    Реализовали 1 часть Data Mapper

---
+++ src/repositories/mappers/base.py
@@ -0,0 +1,15 @@
+
+
+
+class DataMapper:
+    db_model = None
+    schema = None
+
+    @classmethod
+    def map_to_domain_entity(cls, db_model):
+        return cls.schema.model_validate(db_model, from_attributes=True)
+
+    @classmethod
+    def map_to_persistence_entity(cls, data):
+        return cls.db_model(**data.model_dump())
+
