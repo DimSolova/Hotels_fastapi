b90fad5acc6adc2c0280203c6c5387e247e7e545
Solova Mac <dima@MacBook-Air-Dima.local>
2026-03-13 17:06:20 +0300

    Реализовали Работу с Celery Проверка бронирований на текущий день

--- src/database.py
+++ src/database.py
@@ -1,12 +1,16 @@
 #src/database
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 from src.config import setting
 
 engine = create_async_engine(setting.DB_URL)
+engine_null_pool = create_async_engine(setting.DB_URL, poolclass=NullPool)
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+async_session_maker_null_pool = async_sessionmaker(bind=engine_null_pool, expire_on_commit=False)
+
 
 class Base(DeclarativeBase):
     pass
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,10 +1,14 @@
+import asyncio
 import os.path
 
 from PIL import Image
 from pathlib import Path
 from time import sleep
 
+from src.api.dependencies import DBDep
+from src.database import async_session_maker_null_pool
 from src.tasks.celery_app import celery_instance
+from src.utils.db_manager import DBManager
 
 
 @celery_instance.task
@@ -39,3 +43,13 @@ def resize_image(image_path: str):
         img_resized.save(output_path)
 
     print(f"Изображение сохранено в следующих размерах: {sizes} в папке {output_folder}")
+
+async def get_bookings_with_today_checkin_helper():
+    print("celery started")
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        bookings = await db.bookings.get_bookings_with_today_checkin()
+        print(f"{bookings=}")
+
+@celery_instance.task(name="booking_today_checkin")
+def send_emails_to_users_with_today_checkin():
+    asyncio.run(get_bookings_with_today_checkin_helper())
