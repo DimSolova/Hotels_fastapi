282724dbe3b791482df16a9ebe3d00b0003fbb9a
Solova Mac <dima@MacBook-Air-Dima.local>
2026-03-18 18:04:32 +0300

    Исправил Задание 19 по курсу

--- test/conftest.py
+++ test/conftest.py
@@ -3,11 +3,15 @@ import json
 import pytest
 
 from src.config import setting
-from src.database import Base, engine, engine_null_pool
+from src.database import Base, engine, engine_null_pool, async_session_maker_null_pool
 from src.main import app
 from src.models import *
 
 from httpx import ASGITransport, AsyncClient
+
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomAdd
+from src.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session",autouse=True)
@@ -20,24 +24,19 @@ async def setup_database(check_test_mode) -> None:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
-    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-        # Отели
-        with open("test/mock_hotels.json", encoding="utf-8") as f:
-            hotels = json.load(f)
-        for hotel in hotels:
-            await ac.post("/hotels", json=hotel)
+    with open("test/mock_hotels.json", encoding="utf-8") as f:
+        hotels_lst = json.load(f)
 
-        # Комнаты
-        with open("test/mock_rooms.json", encoding="utf-8") as f:
-            rooms = json.load(f)
+    with open("test/mock_rooms.json", encoding="utf-8") as f:
+        rooms_lst = json.load(f)
 
-        for room in rooms:
-            hotel_id = room.pop("hotel_id")  # убираем, если есть
-            url = f"/hotels/{hotel_id}/rooms"
-            await ac.post(
-                url,  # ← вот здесь главное
-                json=room
-            )
+    hotels = [HotelAdd(**hotel) for hotel in hotels_lst]
+    rooms = [RoomAdd(**room) for room in rooms_lst]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(hotels)
+        await db.rooms.add_bulk(rooms)
+        await db.commit()
 
 
 @pytest.fixture(scope="session",autouse=True)
